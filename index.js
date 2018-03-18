e952a2b59f754c712c13f9653693931877cc5be6
Tyler Long <tyler4long@gmail.com>
2018-03-18 12:37:31 +0800

    Check data lines

--- index.js
+++ index.js
@@ -3,14 +3,27 @@ import Vue from 'vue/dist/vue.js'
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
-    numbers: `
+    numbers: ''
+  },
+  computed: {
+    result: function () {
+      const lines = this.numbers.split(/(?:\s*\n\s*)+/).filter(line => line.length > 0)
+      if (lines.length < 4) {
+        return '至少要四行数据'
+      }
+      for (const line of lines) {
+        console.log(line)
+      }
+      return lines
+    }
+  }
+})
+
+// below is for testing purpose
+
+app.numbers = `
 1,6,7,5,3,2,4,10,8,9
 8,6,7,3,5,9,1,2,4,10
 5,3,4,6,8,2,7,1,10,9
 6,5,9,7,4,10,8,2,1,3
-    `.trim()
-  }
-})
-
-console.log(app)
+`.trim()
