0395c0d132cf621488d73637b0523dca956421b1
Tyler Long <tyler4long@gmail.com>
2018-03-18 10:38:45 +0800

    bind data to textarea

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ import Vue from 'vue/dist/vue.js'
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    numbers: `
+1,6,7,5,3,2,4,10,8,9
+8,6,7,3,5,9,1,2,4,10
+5,3,4,6,8,2,7,1,10,9
+6,5,9,7,4,10,8,2,1,3
+    `.trim()
   }
 })
 
