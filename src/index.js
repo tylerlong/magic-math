325d17d26fb4d6186aff2c2a8453d304a16e1501
Tyler Long <tyler4long@gmail.com>
2018-03-25 17:28:07 +0800

    Finish implementation according to new requirements

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const app = new Vue({
   el: '#app',
   data: {
     input: '',
-    start: 1
+    start: 1,
+    count: 4
   },
   computed: {
     result: function () {
@@ -13,21 +14,25 @@ const app = new Vue({
       if (result === false) {
         return data
       }
-      let numbers = data
+      let lines = data
 
       const start = parseInt(this.start)
-      if (start < 1 || start + 3 > numbers.length) {
+      const count = parseInt(this.count)
+      if (count < 2) {
+        return '至少要比较2行'
+      }
+      if (start < 1 || start + count - 1 > lines.length) {
         return '选择的行超出了数据范围'
       }
 
-      numbers = numbers.slice(start - 1, start + 3)
-      console.log(numbers)
-      const matches = findMatches(numbers)
+      lines = lines.slice(start - 1, start + count - 1)
+      console.log(lines)
+      const matches = findMatches(lines)
 
-      let str = '比较的 4 行数据为：\n'
+      let str = `比较的 ${count} 行数据为：\n`
       str += '1	2	3	4	5	6	7	8	9	10\n'
       str += '==================================================================================\n'
-      str += numbers.map(line => line.join('\t')).join('\n')
+      str += lines.map(line => line.join('\t')).join('\n')
       str += '\n\n比较结果为：\n'
       str += '==================================================================================\n'
       str += matches.map(match => `第 ${match[0]}、${match[1]}、${match[2]} 列根据第 ${match[3]} 列筛选得出结果：${match[4]}`).join('\n')
